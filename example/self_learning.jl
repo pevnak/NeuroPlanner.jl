bdb60e5dc23a25db9abd2e453024655023e9d2e0
pevnak <pevnak@gmail.com>
2022-11-16 17:31:33 +0100

    upgraded tracking of best solution and added lstar loss

--- example/self_learning.jl
+++ example/self_learning.jl
@@ -7,70 +7,12 @@ using SymbolicPlanners
 using Statistics
 using IterTools
 using Random
+using StatsBase
+using Serialization
 
 benchdir(s...) = joinpath("benchmarks","blocks-slaney",s...)
 taskfiles(s) = [benchdir(s, f) for f in readdir(benchdir(s)) if startswith(f,"task",)]
-
-function solve_problem(pddld, problem::GenericProblem, model; max_time=30)
-	domain = pddld.domain
-	pddle, state = PDDL2Graph.initproblem(pddld, problem)
-	goal = PDDL.get_goal(problem)
-	planner = AStarPlanner(GNNHeuristic(pddld, problem, model); max_time, save_search = true)
-	solution_time = @elapsed sol = planner(domain, state, goal)
-	sol.status == :success || return(nothing)
-	stats = (;solution_time, 
-		sol_length = length(sol.trajectory),
-		expanded_states = 
-		)
-	(;sol, stats)
-end
-
-solve_problem(pddld, problem_file::AbstractString, model; kwargs...) = solve_problem(pddld, load_problem(problem_file), model;kwargs...)
-
-
-function update_solutions!(solutions, pddld, model, problem_files; offset = 1, stop_after=32)
-	@assert length(solutions) == length(problem_files)
-	updated = Int[]
-	init_offset = offset
-	while true 
-		i = offset
-		problem = load_problem(problem_files[i])
-		newsol = solve_problem(pddld, problem, model)
-		oldsol = solutions[i]
-		solutions[i] = update_solution(oldsol, newsol, pddld, problem)
-		solutions[i] != oldsol && push!(updated, i)
-		offset = offset == length(solutions) ? 1 : offset+1
-		init_offset == offset && break
-		length(updated) ≥ stop_after && break
-	end
-	offset, updated
-end
-
-update_solution(oldsol::Nothing, newsol::Nothing, pddld, problem) = nothing
-update_solution(oldsol::NamedTuple, newsol::Nothing, pddld, problem) = oldsol
-update_solution(oldsol::Nothing, newsol::PathSearchSolution, pddld, problem) = prepare_minibatch_l2(newsol, pddld, problem)
-function update_solution(oldsol::NamedTuple, newsol::PathSearchSolution, pddld, problem) 
-	length(oldsol.y) ≤ length(newsol.trajectory) ? oldsol : prepare_minibatch_l2(newsol, pddld, problem)
-end
-function update_solution(oldsol::NamedTuple, newsol::NamedTuple, pddld, problem) 
-	length(oldsol.y) ≤ length(newsol.trajectory) ? oldsol : merge(prepare_minibatch_l2(newsol.sol, pddld, problem), (;stats = newsol.stats))
-end
-update_solution(oldsol::Nothing, newsol::NamedTuple, pddld, problem) = merge(prepare_minibatch_l2(newsol.sol, pddld, problem), (;stats = newsol.stats))
-function prepare_minibatch_l2(sol, pddld, problem)
-   pddle, state = PDDL2Graph.initproblem(pddld, problem)
-   (;x = PDDL2Graph.sparsegraph(reduce(cat, map(pddle, sol.trajectory))),
-     y = collect(length(sol.trajectory):-1:1),
-     )
-end
-
-function show_stats(solutions)
-	solved = filter(!isnothing, solutions)
-	mean_time = mean(s.solution_time for s in solved)
-	mean_length = mean(length(s.y) for s in solved)
-	println(" solved instances: ", length(solved), 
-		" (",length(solved) / length(solutions), ") mean length ",mean_length, " mean_time = ", mean_time)
-end
-
+include("solution_tracking.jl")
 ######
 # define a NN based solver
 ######
@@ -83,40 +25,72 @@ GNNHeuristic(pddld, problem, model) = GNNHeuristic(PDDL2Graph.add_goalstate(pddl
 Base.hash(g::GNNHeuristic, h::UInt) = hash(g.model, hash(g.pddle, h))
 SymbolicPlanners.compute(h::GNNHeuristic, domain::Domain, state::State, spec::Specification) = only(h.model(h.pddle(state)))
 
-
 create_example(pddld, filename::String) = create_example(pddld, load_problem(filename))	
 create_examples(pddld, dname::String) = [create_example(pddld, f) for f in taskfiles(dname)]
 
 domain = load_domain(benchdir("domain.pddl"))
 pddld = PDDLExtractor(domain)
-problem_files = mapreduce(taskfiles, vcat, ["blocks3", "blocks4", "blocks5", "blocks6", "blocks7", "blocks8"])
+problem_files = mapreduce(taskfiles, vcat, ["blocks$(i)" for i in 3:15])
 solutions = Vector{Any}(fill(nothing, length(problem_files)))
 
-#crate model from some problem instance
+#create model from some problem instance
 model = let 
-	problem = load_problem(benchdir("blocks3", "task01.pddl"))
+	problem = load_problem(benchdir("blocks10", "task01.pddl"))
 	pddle, state = initproblem(pddld, problem)
 	h₀ = pddle(state)
-	odim_of_graph_conv = 4
-	MultiModel(h₀, odim_of_graph_conv, d -> Chain(Dense(d, 32,relu), Dense(32,1)))
+	odim_of_graph_conv = 8
+	model = MultiModel(h₀, odim_of_graph_conv, d -> Chain(Dense(d, 32,relu), Dense(32,1)))
+	solve_problem(pddld, problem, model)	#warmup the solver
+	model
 end
 
-loss(x, y) = Flux.Losses.mse(vec(model(x)), y)
-loss(xy::NamedTuple) = loss(xy.x, xy.y)
-loss(xy::Tuple) = loss(xy[1],xy[2])
+# l₂loss(x, y) = Flux.Losses.mse(vec(model(x)), y)
+# l₂loss(xy::NamedTuple) = l₂loss(xy.x, xy.y)
+# l₂loss(xy::Tuple) = l₂loss(xy[1],xy[2])
+# fminibatch = prepare_minibatch_l2
+"""
+lₛloss(x, g, H₊, H₋)
+
+Minimizes `L*` loss, We want ``f * H₋ .< f * H₊``, which means to minimize cases when ``f * H₋ .> f * H₊``
+"""
+function lₛloss(x, g, H₊, H₋)
+	g = reshape(g, 1, :)
+	f = model(x) .+ g
+	mean(softplus.(f * H₋ .- f * H₊))
+end
+lₛloss(xy::NamedTuple) = lₛloss(xy.x, xy.path_cost, xy.H₊, xy.H₋)
+nonempty(s::NamedTuple) = !isempty(s.H₊) && !isempty(s.H₋)
+fminibatch = prepare_minibatch_lₛ
+loss = lₛloss
 offset = 1
 opt = AdaBelief()
 ps = Flux.params(model)
+all_solutions = []
+losses = []
+for i in 1:10
+	offset = 1
+	while offset < length(solutions)
+		offset, updated = update_solutions!(solutions, pddld, model, problem_files, fminibatch; offset, solve_solved = true, stop_after=32)	
+		solved = findall(solutions .!== nothing)
+		print("offset = ", offset," updated = ", length(updated), " ")
+		show_stats(solutions)
+		#do one epoch on newly solved instances
+		updated_solutions = filter(nonempty, solutions[updated])
+		t₁ = @elapsed length(updated) > 0 && Flux.train!(loss, ps, updated_solutions, opt)
+		#do one epoch on all solved instances so far
+		# t₂ = @elapsed Flux.train!(loss, ps, solutions[ii], opt)		
+		#do one epoch on all solved instances but prioriteze those with the largest number of expanded nodes
+		solved = solutions[solved];
+		solved = filter(nonempty, solved);
 
-for i in 1:100
-	offset, updated = update_solutions!(solutions, pddld, model, problem_files; offset)	
-	solved = findall(solutions .!== nothing)
-	ii = solved[randperm(length(solved))]
-	print("offset = ",offset," ")
-	show_stats(solutions)
-	#do one epoch on newly solved instances
-	t₁ = @elapsed Flux.train!(loss, ps, solutions[updated], opt)
-	#do one epoch on all solved instances so far
-	t₂ = @elapsed Flux.train!(loss, ps, solutions[ii], opt)
-	@show (t₁, t₂)
+		# we should actually 
+		w = StatsBase.Weights([s.stats.expanded for s in solved]);
+		t₂ = @elapsed Flux.train!(loss, ps, repeatedly(() -> sample(solved, w), 1000), opt)
+		@show (t₁, t₂)
+	end
+	l = [loss(s) for s in solutions if s !== nothing && nonempty(s)]
+	push!(losses, l)
+	println("loss after $(i) epoch = ", mean(l))
+	push!(all_solutions, [(s == nothing ? nothing : s.stats) for s in solutions])
+	serialize("stats_ls.jls",(;all_solutions, losses))
 end
---
+++ example/solution_tracking.jl
@@ -0,0 +1,141 @@
+using Term
+using SparseArrays
+using OneHotArrays
+verbose::Bool = true
+
+function solve_problem(pddld, problem::GenericProblem, model; max_time=30)
+	domain = pddld.domain
+	pddle, state = PDDL2Graph.initproblem(pddld, problem)
+	goal = PDDL.get_goal(problem)
+	planner = AStarPlanner(GNNHeuristic(pddld, problem, model); max_time, save_search = true)
+	solution_time = @elapsed sol = planner(domain, state, goal)
+	sol.status == :success || return(nothing)
+	stats = (;solution_time, 
+		sol_length = length(sol.trajectory),
+		expanded = sol.expanded,
+		)
+	(;sol, stats)
+end
+
+solve_problem(pddld, problem_file::AbstractString, model; kwargs...) = solve_problem(pddld, load_problem(problem_file), model;kwargs...)
+
+
+function update_solutions!(solutions, pddld, model, problem_files, fminibatch; offset = 1, stop_after=32, cycle = false, solve_solved = false)
+	@assert length(solutions) == length(problem_files)
+	updated_solutions = Int[]
+	init_offset = offset
+	while true 
+		i = offset
+		problem = load_problem(problem_files[i])
+		oldsol = solutions[i]
+		if solve_solved || oldsol == nothing
+			newsol = solve_problem(pddld, problem, model)
+			solutions[i] = update_solution(oldsol, newsol, pddld, problem, fminibatch)
+			solutions[i] != oldsol && push!(updated_solutions, i)
+		end
+		!cycle && offset == length(solutions) && break 
+		offset = offset == length(solutions) ? 1 : offset+1
+		init_offset == offset && break
+		length(updated_solutions) ≥ stop_after && break
+	end
+	offset, updated_solutions
+end
+
+update_solution(oldsol::Nothing, newsol::Nothing, pddld, problem, fminibatch) = nothing
+update_solution(oldsol::Nothing, newsol::PathSearchSolution, pddld, problem, fminibatch) = fminibatch(newsol, pddld, problem, fminibatch)
+function update_solution(oldsol::NamedTuple, newsol::PathSearchSolution, pddld, problem, fminibatch) 
+	length(oldsol.stats.sol_length) ≤ length(newsol.trajectory) ? oldsol : fminibatch(newsol, pddld, problem, fminibatch)
+end
+
+function update_solution(oldsol::NamedTuple, newsol::NamedTuple, pddld, problem, fminibatch)
+	o, n = oldsol.stats.sol_length,  newsol.stats.sol_length
+	if o < n
+		print(@red "longer: $(o) -> $(n)  ")
+	elseif o > n
+		print(@green "shorter: $(o) -> $(n)  ")
+	else
+		print(@yellow "same length: $(o) -> $(n)  ")
+	end
+
+	o, n = oldsol.stats.expanded,  newsol.stats.expanded
+	if o < n
+		println(@red "more states: $(o) -> $(n)")
+	elseif o > n
+		println(@green "less states: $(o) -> $(n)")
+	else
+		println(@yellow "same states: $(o) -> $(n)")
+	end
+	stats = (;solution_time = newsol.stats.solution_time,
+		sol_length = min(oldsol.stats.sol_length, newsol.stats.sol_length),
+		expanded = newsol.stats.expanded,
+		)
+	length(oldsol.y) ≤ length(newsol.sol.trajectory) ? oldsol : merge(fminibatch(newsol.sol, pddld, problem), (;stats))
+end
+
+function update_solution(oldsol::Nothing, newsol::NamedTuple, pddld, problem, fminibatch) 
+	verbose && println(@green "new solution: $((newsol.stats.sol_length, newsol.stats.expanded))")
+	merge(fminibatch(newsol.sol, pddld, problem), (;stats = newsol.stats))
+end
+
+function update_solution(oldsol::NamedTuple, newsol::Nothing, pddld, problem, fminibatch)
+	verbose && println(@red "new is unsolved: $((oldsol.stats.sol_length, oldsol.stats.expanded))")
+	stats = (;solution_time = oldsol.stats.solution_time,
+	sol_length = oldsol.stats.sol_length,
+	expanded = 10*oldsol.stats.expanded, # let's try to increase priority of this poor guy
+	)
+	(;x = oldsol.x, y = oldsol.y, stats = stats)
+end
+
+function prepare_minibatch_l2(sol, pddld, problem)
+   pddle, state = PDDL2Graph.initproblem(pddld, problem)
+   (;x = PDDL2Graph.sparsegraph(reduce(cat, map(pddle, sol.trajectory))),
+     y = collect(length(sol.trajectory):-1:1),
+     )
+end
+
+function prepare_minibatch_lₛ(sol, pddld, problem)
+	sol.search_tree === nothing && error("solve the problem with `save_search=true` to keep the search tree")
+	pddle, state = PDDL2Graph.initproblem(pddld, problem)
+
+	# get indexes of the states on the solution path, which seems to be hashes of states 
+	trajectory_id = hash.(sol.trajectory)
+	child₁ = off_path_childs(sol, trajectory_id)
+	# child₂ = off_path_childs(sol, union(child₁, trajectory_id))
+	ids = vcat(trajectory_id, child₁)
+	path_cost = [sol.search_tree[i].path_cost for i in ids]
+	states = [sol.search_tree[i].state for i in ids]
+	# we want every state on the solution path to be smaller than  
+	pm = [(i,j) for i in 1:length(trajectory_id) for j in length(trajectory_id)+1:length(ids)]
+	H₊ = onehotbatch([i[2] for i in pm], 1:length(ids))
+	H₋ = onehotbatch([i[1] for i in pm], 1:length(ids))
+
+	(;x = PDDL2Graph.sparsegraph(reduce(cat, map(pddle, states))),
+         sol_length = length(trajectory_id), H₊, H₋, path_cost)
+end
+
+off_path_childs(sol, parents_id::Vector) = off_path_childs(sol, Set(parents_id))
+
+function off_path_childs(sol, parents_id::Set)
+	childs = [s.id for s in values(sol.search_tree) if s.parent_id ∈ parents_id]
+	setdiff(childs, parents_id)
+end
+
+function show_stats(solutions)
+	solved = filter(!isnothing, solutions)
+	stats = [s.stats for s in solved]
+	mean_time = round(mean(s.solution_time for s in stats), digits = 2)
+	mean_length = round(mean(s.sol_length for s in stats), digits = 2)
+	mean_expanded = round(mean(s.expanded for s in stats), digits = 2)
+	println(" solved instances: ", length(solved), 
+		" (",round(length(solved) / length(solutions), digits = 2), ") mean length ",mean_length, " mean expanded ",mean_expanded, " mean_time = ", round(mean_time, digits = 3))
+end
+
+function _show_stats(stats)
+	solved = filter(!isnothing, stats)
+	mean_time = round(mean(s.solution_time for s in solved), digits = 2)
+	mean_length = round(mean(s.sol_length for s in solved), digits = 2)
+	mean_expanded = round(mean(s.expanded for s in solved), digits = 2)
+	mean_excess = round(mean(s.expanded ./ s.sol_length for s in solved), digits = 2)
+	println(" solved instances: ", length(solved), 
+		" (",round(length(solved) / length(stats), digits = 2), ") mean length ",mean_length, " mean expanded ",mean_expanded, " mean expanded excess ",mean_excess, " mean_time = ", round(mean_time, digits = 3))
+end
