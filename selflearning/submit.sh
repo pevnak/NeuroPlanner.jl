089e417599ba9b73a73839532e7611f7582d2c4a
pevnak <pevnak@gmail.com>
2023-02-19 21:33:24 +0100

    experiment

--- selflearning/submit.sh
+++ selflearning/submit.sh
@@ -1,7 +1,9 @@
 for ((i = 1;i<=1;i++)) ; do
-	for p in blocks  ; do
+	# for p in blocks  ; do
 	# for p in blocks ferry gripper npuzzle ; do
-		for h in HAdd HMax null ; do
+	for p in blocks; do
+		# for h in HAdd HMax null ; do
+		for h in null ; do
 			sbatch -D /home/pevnytom/julia/Pkg/NeuroPlanner.jl/selflearning slurm.sh $p lstar $h $i
 			# sbatch -D /home/pevnytom/julia/Pkg/NeuroPlanner.jl/selflearning slurm.sh $p lgbfs $h $i
 			sbatch -D /home/pevnytom/julia/Pkg/NeuroPlanner.jl/selflearning slurm.sh $p l2 $h $i
